11e55899293425390e1f879c595849861ea516d2
Amirhesam <amirhesamshokri@gmail.com>
2023-11-30 18:04:38 +0330

    setup react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h1> this is the main part</h1>
+        <Container className='py-5'>
+          <h1> this is the main part</h1>
+        </Container>
       </main>
       <Footer />
     </div>
