abb56660887ea07349ae2e746bff93fb8c24dae6
Amirhesam <amirhesamshokri@gmail.com>
2023-12-01 16:32:57 +0330

    sync django api to react by axios

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,12 +40,15 @@ INSTALLED_APPS = [
     
     #3rd-party apps
     'rest_framework',
+    'corsheaders',
     
     #my apps
     "base",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -127,3 +130,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+)
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,43 @@
-import React from 'react'
-import { Row , Col } from 'react-bootstrap'
-import products from '../products'
-import Product from '../components/Product'
-
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const { data } = await axios.get(
+          "http://127.0.0.1:8000/api/v1/products/"
+        );
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error.message); // Log the specific error message
+        setError("Error fetching products. Please try again later.");
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div>
-        <h1> latest products</h1>
-        <Row>
-            {products.map(product => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product}/>
-                </Col>
-            ))}
-        </Row>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
